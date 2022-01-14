ce2c9a90fd77f70975ee149235a3f3cfbe9a1f6c
Arturs <arrturs.torsters@gmail.com>
2022-01-14 22:38:27 +0200

    done

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 "use strict";
-// the story with possible replacements
-let originalStory =
-  "Once upon a ${userInputs[0]}, there was a ${userInputs[1]} hearted, ${userInputs[3]} dragon that was so mean and ${userInputs[4]} ";
+
 
 // how many questions
 let numberOfQuestions = 4;
 //what questions should appear
 let questionArray = [
-  "Enter name",
-  "Enter something",
-  "Enter noun",
-  "Enter kartupelis",
+  "Ka Tevi sauc?",
+  "Cik Tev gadi ?",
+  "Tu lohs esi ?",
+  "Diršana"
 ];
 //which question iam on
 let questionCounter = 0;
@@ -20,8 +18,19 @@ let userInputs = [];
 // console.log(`The "i" of  "for" loop `);
 for (let i = numberOfQuestions; i >= 0; i--) {
   console.log(i);
-  console.log(questionArray);
+  //print the questions + how many left to answer
+  userInputs.push(prompt(questionArray[questionCounter] + `(${numberOfQuestions--} questions left)`));
   //current "questionCounter" variable (it should go up)
   questionCounter++;
   console.log(questionCounter);
 }
+// the story with possible replacements
+let originalStory = `<h2>Sveiks ${userInputs[0]} ! Skatos ,ka Tev ir  ${userInputs[1]} gadu, ${userInputs[3]} es jau ta ari domaju! ${userInputs[4]} </h2>`;
+
+ // End of User Inputs
+ alert(
+    "All done! Ready for your totally-accurate, not-at-all confusing history of tech??"
+  );
+
+  console.log(originalStory);
+  document.write(originalStory);
