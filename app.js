1d92ff4a108ff19c7fe7adf800e2574155862666
Arturs <arrturs.torsters@gmail.com>
2022-01-14 21:05:50 +0200

    Added mad-lib replacement variables to story

---
+++ app.js
@@ -0,0 +1,41 @@
+"use strict";
+let userInputs = [];
+    
+
+
+let originalStory = 'Once upon a ${userInputs[0]}, there was a ${userInputs[1]} hearted, ${userInputs[2]} dragon that was so mean and ${userInputs[2]} ';
+
+
+
+console.log(originalStory);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  "Once upon a time, there was a cold hearted, backstabbing dragon that was so mean and ugly. He loved to travel to the human world and snatch littlekids out of their beds while they were sleeping, and he would drop themdown his green tortoise throat. Now back in those days all the kids wouldsit in his stomach and cry, Let us out, Let us out. Of course he would notlet them out, but sometimes a brave little soul would travel the long journeytill he plopped out the other end";
+
