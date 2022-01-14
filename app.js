524d42afe539fba4d94955c05f2a3e5d20c96321
Arturs <arrturs.torsters@gmail.com>
2022-01-14 22:14:36 +0200

    g

--- app.js
+++ app.js
@@ -1,63 +1,27 @@
 "use strict";
 // the story with possible replacements
-let originalStory = 'Once upon a ${userInputs[0]}, there was a ${userInputs[1]} hearted, ${userInputs[3]} dragon that was so mean and ${userInputs[4]} ';
+let originalStory =
+  "Once upon a ${userInputs[0]}, there was a ${userInputs[1]} hearted, ${userInputs[3]} dragon that was so mean and ${userInputs[4]} ";
 
 // how many questions
 let numberOfQuestions = 4;
 //what questions should appear
-let questionArray = ['Enter name', 'Enter something', 'Enter name', 'Enter name'];
+let questionArray = [
+  "Enter name",
+  "Enter something",
+  "Enter noun",
+  "Enter kartupelis",
+];
 //which question iam on
 let questionCounter = 0;
 //user inputs
 let userInputs = [];
 
-for  (let i = numberOfQuestions ; i >= 0; i--) 
-{ 
-    console.log(i);
-    questionCounter++;
-    console.log(questionCounter);
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  "Once upon a time, there was a cold hearted, backstabbing dragon that was so mean and ugly. He loved to travel to the human world and snatch littlekids out of their beds while they were sleeping, and he would drop themdown his green tortoise throat. Now back in those days all the kids wouldsit in his stomach and cry, Let us out, Let us out. Of course he would notlet them out, but sometimes a brave little soul would travel the long journeytill he plopped out the other end";
-
+// console.log(`The "i" of  "for" loop `);
+for (let i = numberOfQuestions; i >= 0; i--) {
+  console.log(i);
+  console.log(questionArray);
+  //current "questionCounter" variable (it should go up)
+  questionCounter++;
+  console.log(questionCounter);
+}
